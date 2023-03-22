9df65dc9b49ba8b38ef691658f45d46c8f62344a
devashish-deepsource <devashish@deepsource.io>
2023-03-22 21:43:42 +0530

    Add issue wala code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
-public class Main {
+public class Main implements Comparable<Main> {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+
+    @Override
+    public int compareTo(Main o) {
+        double d1 = Math.random();
+        double d2 = Math.random();
+
+        // if (d1 == d2) {
+        if (d1 == d2) {
+            // Vibe here.
+            return 0;
+        }
+        return 1;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
     }
 }
