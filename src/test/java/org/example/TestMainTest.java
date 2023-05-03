5d7cdc71534a5388e8c34e6a684a239ea2e0edfb
devashish-deepsource <devashish@deepsource.io>
2023-05-04 02:55:48 +0530

    Change name of the test file

--- src/test/java/org/example/TestMainTest.java
+++ src/test/java/org/example/TestMainTest.java
@@ -2,9 +2,7 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-public class MainTest {
+public class TestMainTest {
     @Test
     public void shouldNotBePublic() {
         // `public` access modifier should be removed from the class and this method.
