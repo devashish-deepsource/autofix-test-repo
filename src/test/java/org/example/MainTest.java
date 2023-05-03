7ce24b3bd3817696f10ed466690caa7cf97e873e
devashish-deepsource <devashish@deepsource.io>
2023-05-04 02:48:12 +0530

    Add two more tests

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+    @Test
+    public void shouldNotBePublic() {
+        // `public` access modifier should be removed from the class and this method.
+    }
+
+    @Test
+    public void somethingJapanese() {
+        // これはコメントです。
+    }
+}
